8e3f535b595b15796b0d3f8bfbfbb2f6ebaf5bc7
J1ngxuanWei <wjx021002@gmail.com>
2024-05-14 11:03:29 +0000

    time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,11 +1,12 @@
 use crate::{
     config::MAX_SYSCALL_NUM,
     fs::{open_file, OpenFlags},
-    mm::{translated_ref, translated_refmut, translated_str},
+    mm::{translated_byte_buffer, translated_ref, translated_refmut, translated_str},
     task::{
         current_process, current_task, current_user_token, exit_current_and_run_next, pid2process,
         suspend_current_and_run_next, SignalFlags, TaskStatus,
     },
+    timer::get_time_us,
 };
 use alloc::{string::String, sync::Arc, vec::Vec};
 
@@ -162,12 +163,31 @@ pub fn sys_kill(pid: usize, signal: u32) -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
+use core::mem::size_of;
+#[allow(unused)]
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().process.upgrade().unwrap().getpid()
-    );
-    -1
+    let us = get_time_us();
+    unsafe {
+        let mut vvv = translated_byte_buffer(
+            current_user_token(),
+            (_ts as *const u8),
+            size_of::<TimeVal>(),
+        );
+        let mut buffer = [0u8; size_of::<TimeVal>()];
+
+        let rptr = buffer.as_mut_ptr() as usize as *mut TimeVal;
+        *rptr = TimeVal {
+            sec: us / 1_000_000,
+            usec: us % 1_000_000,
+        };
+
+        for vv in vvv.iter_mut() {
+            for i in 0..vv.len() {
+                vv[i] = buffer[i];
+            }
+        }
+    }
+    0
 }
 
 /// task_info syscall
